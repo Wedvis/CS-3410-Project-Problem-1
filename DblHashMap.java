e828da3829ba01081487c59a1a9e191d6293ebff
Guarded-bagel <jbachrach45@gmail.com>
2026-04-30 17:20:05 -0400

    Add files via upload

--- DblHashMap.java
+++ DblHashMap.java
@@ -2,18 +2,20 @@ package group_project;
 
 
 public class DblHashMap<T> implements StringHashMap<T> {
-//this is the testing file, please use with proper string interfaces for this to work
-//Note that this hash table does not automatically resize, you have to call resizePrime(), or resize(array.length * 2) once the array reaches half capacity
+
     public StringKeyVal<T>[] array;
     public int valCount = 0;
     public boolean isResize = false;
     public int collisions = 0;
     public int probes_put = 0;
+    public int gets = 0;
     public int probes_find = 0;
+    public int finds_occuring = 0;
 
     public DblHashMap(int size) {
         array = new StringKeyVal[size];
     }
+
 
     //Resize function. Takes all of the values in the HashMap and throws them into a temp array, increases the size
     //of the internal array, and then uses put to place everything back into the array.
@@ -25,7 +27,7 @@ public class DblHashMap<T> implements StringHashMap<T> {
         int j = 0;
 
         for(int i = 0; i < array.length; ++i) {
-            if(array[i] != null) {
+            if(array[i] != null && array[i].getVal()!=null) {
                 tempArray[j] =  array[i];
                 j++;
             }
@@ -34,6 +36,8 @@ public class DblHashMap<T> implements StringHashMap<T> {
         array = new StringKeyVal[size];
 
         for(int i = 0; i < tempArray.length; i++) {
+            if(tempArray[i]==null)
+                break;
             put(tempArray[i].getVal(), tempArray[i].getkey());
         }
         isResize = false;
@@ -51,7 +55,7 @@ public class DblHashMap<T> implements StringHashMap<T> {
         int j = 0;
 
         for(int i = 0; i < array.length; ++i) {
-            if(array[i] != null) {
+            if(array[i] != null && array[i].getVal()!=null) {
                 tempArray[j] =  array[i];
                 j++;
             }
@@ -60,6 +64,8 @@ public class DblHashMap<T> implements StringHashMap<T> {
         array = new StringKeyVal[newSize];
 
         for(int i = 0; i < tempArray.length; i++) {
+            if(tempArray[i]==null)
+                break;
             put(tempArray[i].getVal(), tempArray[i].getkey());
         }
 
@@ -72,10 +78,11 @@ public class DblHashMap<T> implements StringHashMap<T> {
     @Override
     public long hashfunction(String key) {
         HashFunctions hf = new HashFunctions();
-        if(hf.fnv1(key) < 0) {
-            return -1 * hf.fnv1(key);
+        long hash = hf.djb2(key);
+        if(hash < 0) {
+            return -1 * hash;
         }
-        return hf.fnv1(key);
+        return hash;
     }
 
     //Secondary hash function, also uses djb2, multiplies by a different prime number.
@@ -84,25 +91,34 @@ public class DblHashMap<T> implements StringHashMap<T> {
     @Override
     public long hash2(String key) {
        HashFunctions hf = new HashFunctions();
-        if(hf.fnv1a(key) < 0) {
-            return -1 * hf.fnv1a(key);
+       //long hash = hashfunction(key);
+       long hash = hf.djb2_dbl(key);
+        if(hash < 0) {
+            return -1 * hash;
         }
-       return hf.fnv1a(key);
+       return hash;
     }
 
     //Helper for put, probes for next spot to place the value. Does so by using secondary hash function,
     //and then adding 1 for every subsequent position in the array, to scan the whole array for a place.
     private void probe(String key, StringKeyVal<T> keyval) {
-        //long newHash = 17 - (hash2(key) % 17);
-        long newHash = hash2(key);
-        //long oldHash = hashfunction(key);
+        long newHash = 17 - (hash2(key) % 17);
+        if(newHash == array.length) {
+            newHash -= 1;
+        }
+        long oldHash = hashfunction(key);
+
+//        long newHash = hash2(key);
         boolean noPlace = true;
 
-        for(int i = 0; i < array.length; i++) {
-            //int index = ((int)((oldHash + (i * newHash)) % array.length));
-            ++probes_put;
-            int index = ((int)((newHash + i) % array.length));
-            //Replace above line of code with commented line if you want to use prime numbers for table size
+        for(int i = 1; i < array.length; i++) {
+            int index = ((int)((oldHash + (i * newHash)) % array.length));
+            if(!isResize) {
+                ++probes_put;
+            }
+
+//            int index = ((int)((newHash + i) % array.length));
+            //Replace above line of code with commented line if you want to use non prime numbers for table size
 
 
             if(array[index] == null) {
@@ -129,18 +145,20 @@ public class DblHashMap<T> implements StringHashMap<T> {
     //Helper for get, probes just like the probe function, but tries to find the key instead. If not found,
     //returns -1
     private int find(String key) {
+        ++finds_occuring;
+        long newHash = 17 - (hash2(key) % 17);
+        if(newHash == array.length) {
+            newHash -= 1;
+        }
+//        long newHash = hash2(key);
+        long oldHash = hashfunction(key);
 
-        //long newHash = 17 - (hash2(key) % 17);
-        long newHash = hash2(key);
-        //long oldHash = hashfunction(key);
-
-
-
-        for(int i = 0; i < array.length; i++) {
-            //int index = ((int)((oldHash + (i * newHash)) % array.length));
+        for(int i = 1; i < array.length; i++) {
+            int index = ((int)((oldHash + (i * newHash)) % array.length));
+            ++gets;
             ++probes_find;
-            int index = ((int)((newHash + i) % array.length));
-            //replace above code with commented line if you want to use prime table size.
+//            int index = ((int)((newHash + i) % array.length));
+            //replace above code with commented line if you want to use non-prime table size.
 
             if(array[index] != null) {
                 if(key.equals(array[index].getkey())){
@@ -151,6 +169,33 @@ public class DblHashMap<T> implements StringHashMap<T> {
         }
 
         return -1;
+    }
+
+    public StringKeyVal<T> remove(String key) {
+        int index = (int)(hashfunction(key) % array.length);
+        StringKeyVal<T> keyval = array[index];
+
+        if(array[index] == null) {
+            return null;
+        }
+        if (keyval.getkey().equals(key)) {
+            StringKeyVal<T> newkeyval = array[index];
+            array[index] = new StringKeyVal<>(null, key);
+            return newkeyval;
+        }
+        else {
+            int newIndex = find(key);
+            if(newIndex != -1) {
+                StringKeyVal<T> newkeyval = array[newIndex];
+                array[newIndex] = new StringKeyVal<>(null, key);
+                return newkeyval;
+            }
+            else {
+                System.out.println("key not found");
+                return null;
+            }
+        }
+
     }
 
     //Put function, uses first hash function to hash key into a position. If that position is has something,
@@ -178,6 +223,7 @@ public class DblHashMap<T> implements StringHashMap<T> {
                     collisions += 1;
                 }
             }
+
         }
     }
 
@@ -186,6 +232,7 @@ public class DblHashMap<T> implements StringHashMap<T> {
     @Override
     public StringKeyVal<T> get(String key) {
         StringKeyVal<T> keyval = array[(int)(hashfunction(key) % array.length)];
+        ++gets;
 
         if(keyval == null) {
             return null;
@@ -226,4 +273,3 @@ public class DblHashMap<T> implements StringHashMap<T> {
         return true;
     }
 }
-
