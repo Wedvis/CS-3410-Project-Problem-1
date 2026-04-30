50dbe8791ee649611172de8c7ea7d6dbb7aff5b9
Guarded-bagel <jbachrach45@gmail.com>
2026-04-30 17:30:53 -0400

    Update Test_Hashfunction.java

--- Test_Hashfunction.java
+++ Test_Hashfunction.java
@@ -7,25 +7,24 @@ import java.io.PrintWriter;
 public class Test_Hashfunction {
     public static void main(String args[]) {
         File data = new File("src/group_project/data.csv");
-        //File fnv_size11 = new File("src/group_project/fnv_size11.txt");
+        File fnv_size11 = new File("src/group_project/fnv_size11.txt"); //note, ignore variable name, change the file name in URL to reflect data being produced
         boolean doesExist = data.exists();
 
         if(doesExist) {
             try {
                 long t1 = System.nanoTime();
                 Scanner input = new Scanner(data);
-                //PrintWriter writer = new PrintWriter(fnv_size11);
-                //writer.println("valCount,collisions");
+                PrintWriter writer = new PrintWriter(fnv_size11);
+                writer.println("valCount,collisions");
                 input.next();
                 DblHashMap<Integer> test = new DblHashMap<>(3);
                 int value = 0;
                 while(input.hasNext()) {
                     String[] line = input.next().split(",");
-                    if(Double.parseDouble(line[4]) > -7) {
-                        test.put(0, line[0]);
-                        //writer.println(test.valCount + "," + test.collisions);
+                     test.put(0, line[0]);
+                        writer.println(test.valCount + "," + test.collisions);
                         if(test.valCount >= (test.array.length/2)) {
-                            test.resizePrime();
+                            test.resizePrime(); //change to resize(test.array.length * 2) for non prime resizes
                         }
                     }
                 }
@@ -34,7 +33,7 @@ public class Test_Hashfunction {
                 System.out.println(test.collisions);
                 System.out.println(test.valCount);
                 input.close();
-                //writer.close();
+                writer.close();
 
             } catch (IOException e) {
                 System.out.println(":C");
